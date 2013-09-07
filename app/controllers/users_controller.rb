31071baf066ef7eb0661f39dcee9f7ca99c8d9bd
Chitresh Goyal <chitreshstar1@gmail.com>
2013-09-07 18:25:46 +0530

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,28 @@ class UsersController < ApplicationController
   end
   def create
     @user = User.create(params[:user])
-    redirect_to :controller => :users, :action => :index, :flash => "User Created Successfully"
+    redirect_to :controller => :users, :action => :index
     #render :text=>"<pre>"+@user.to_yaml and return
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to :controller => :users, :action => :index, notice: 'User was successfully updated.' }
+        format.xml  { head :ok }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
